8c29ac629e13a97881b593ac4a259fdb5a821aa4
BantuD <500069424@stu.upes.ac.in>
2021-08-16 15:46:55 +0530

    CICD LAB2 MAVEN JENSKINS INTEGRation

---
+++ src/main/java/TestJenkins.java
@@ -0,0 +1,9 @@
+
+public class TestJenkins {
+ public static void main(String arg[])
+ {
+	 System.out.println("Testing maven with Jenkins");
+	 System.out.println("Secound piece of code");
+	 System.out.println("Third piece of code");
+ }
+}
